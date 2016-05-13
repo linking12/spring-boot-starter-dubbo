c527d43ba0fbf6db9887012ba8b50dce5dcf27a9
mingyu.lsm <mingyu.lsm@alibaba-inc.com>
2016-05-13 14:42:22 +0800

    fixbug  两次注册

--- src/main/java/com/alibaba/boot/dubbo/DubboConfigurationApplicationContextInitializer.java
+++ src/main/java/com/alibaba/boot/dubbo/DubboConfigurationApplicationContextInitializer.java
@@ -1,8 +1,6 @@
 package com.alibaba.boot.dubbo;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
@@ -11,32 +9,18 @@ import com.alibaba.dubbo.config.spring.AnnotationBean;
 
 public class DubboConfigurationApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private ConfigurableApplicationContext applicationContext;
-
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
         Environment env = applicationContext.getEnvironment();
         String scan = env.getProperty("spring.dubbo.scan");
         if (scan != null) {
-            AnnotationBean scanner = (AnnotationBean) registerAndInstance(scan);
+            AnnotationBean scanner = BeanUtils.instantiate(AnnotationBean.class);
             scanner.setPackage(scan);
             scanner.setApplicationContext(applicationContext);
             applicationContext.addBeanFactoryPostProcessor(scanner);
             applicationContext.getBeanFactory().addBeanPostProcessor(scanner);
+            applicationContext.getBeanFactory().registerSingleton("annotationBean", scanner);
         }
-    }
-
-    private Object registerAndInstance(String scan) {
-        if (!applicationContext.containsBean(AnnotationBean.class.getName())) {
-            BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(AnnotationBean.class);
-            beanDefinitionBuilder.addPropertyValue("package", scan);
-            beanDefinitionBuilder.addPropertyValue("applicationContext", applicationContext);
-            BeanDefinitionRegistry beanDefinitonRegistry = (BeanDefinitionRegistry) applicationContext.getBeanFactory();
-            beanDefinitonRegistry.registerBeanDefinition(AnnotationBean.class.getName(),
-                                                         beanDefinitionBuilder.getRawBeanDefinition());
-        }
-        return BeanUtils.instantiate(AnnotationBean.class);
 
     }
 
